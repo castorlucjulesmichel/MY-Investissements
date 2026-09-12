324e30ddb4cd3d1d810c445b69ee618d355fa14f
castorlucjulesmichel <castormystro@gmail.com>
2026-09-12 10:56:10 -0400

    Connect MY Investissements to Firebase

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8DoDtvzUkqoMscfe5U_CNwIp5NJXLhMY",
+  authDomain: "my-investissements.firebaseapp.com",
+  projectId: "my-investissements",
+  storageBucket: "my-investissements.firebasestorage.app",
+  messagingSenderId: "399847723113",
+  appId: "1:399847723113:web:91155048944d11e3ad92f8"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
