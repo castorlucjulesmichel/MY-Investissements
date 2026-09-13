913de8bf33b2db02c0833c45200ea99734400368
castorlucjulesmichel <castormystro@gmail.com>
2026-09-13 03:03:23 -0400

    Add service worker for MY Investissements PWA

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'my-investissements-v1';
+const CORE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/auth.js',
+  '/i18n.js',
+  '/firebase-config.js',
+  '/manifest.webmanifest',
+  '/assets/app-icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(request)
+      .then(response => {
+        if (response && response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE).then(cache => cache.put(request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(request).then(cached => cached || caches.match('/index.html')))
+  );
+});
